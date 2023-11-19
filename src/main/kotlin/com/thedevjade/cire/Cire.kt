108877d5cfb1bd38747a33c9a3456b2d25cb7b34
theDevJade <jadeisstolen@gmail.com>
2023-11-19 14:16:31 -0800

    Added Custom Item Stuff!

--- src/main/kotlin/com/thedevjade/cire/Cire.kt
+++ src/main/kotlin/com/thedevjade/cire/Cire.kt
@@ -4,14 +4,28 @@
 
 package com.thedevjade.cire
 
+import com.thedevjade.cire.items.EventProcesser
+import com.thedevjade.cire.items.citem
 import com.thedevjade.cire.resourcepacks.localResourcePack
+import org.bukkit.Material
+import org.bukkit.attribute.Attribute
+import org.bukkit.attribute.AttributeModifier
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.checkerframework.checker.nullness.qual.NonNull
 import org.checkerframework.framework.qual.DefaultQualifier
 
+
+lateinit var plugin: Cire private set
+
 @DefaultQualifier(NonNull::class)
 class Cire : JavaPlugin() {
   override fun onEnable() {
+
+    server.pluginManager.registerEvents(EventProcesser, this)
+
+    plugin = this
     localResourcePack(true) {
       verbose = true
       httpdPort = 8080
@@ -19,6 +33,28 @@ class Cire : JavaPlugin() {
         "pack" to dataFolder.resolve("pack.zip")
       )
     }
+
+    val stackWithListeners = citem(Material.ACACIA_BOAT, "test") {
+      lore {
+        line("Test")
+        line("Test2")
+        line("&aColoredTest")
+      }
+      attributeBuilder(Attribute.GENERIC_MAX_HEALTH) {
+        mode = AttributeModifier.Operation.MULTIPLY_SCALAR_1
+        value = 5.0
+        add()
+      }
+
+      interact { context ->
+        val ev = context.event as PlayerInteractEvent
+        ev.player.sendMessage("Test")
+      }
+
+      setName("&aWOWEEE")
+
+      addEnchantment(Enchantment.DAMAGE_ALL, 5)
+    }.build()
   }
 
   override fun onDisable() {}
